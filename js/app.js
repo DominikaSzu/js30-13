0d5b42f6d93e0d06c45f2575346d7ad425c75bc0
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-06 20:36:23 +0200

    Check if image is on window

---
+++ js/app.js
@@ -0,0 +1,26 @@
+    function debounce(func, wait = 20, immediate = true) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      };
+    }
+
+const slidePhotos = document.querySelectorAll('.slide-in');
+
+function checkScroll(e) {
+    slidePhotos.forEach(slidePhoto => {
+        const slideInAt = (window.scrollY + window.innerHeight) - slidePhoto.height / 2;
+        const slideBottom = slidePhoto.offsetTop + slidePhoto.height;
+        const isHalfShown = slideInAt > slidePhoto.offsetTop;
+    });
+}
+
+window.addEventListener('scroll', debounce(checkScroll));
